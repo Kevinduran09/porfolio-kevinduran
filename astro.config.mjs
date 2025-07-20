7596339599deb96e2a56ff1c34eaa0f087f35d5e
Kevin Andrey Durán Martínez <kevinduran.net.123@gmail.com>
2025-07-20 17:12:02 -0600

    add metadata tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import lottie from "astro-integration-lottie";
-
+import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
-    vite:{
-        plugins:[tailwindcss()]
-    },
-    integrations: [lottie()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [lottie(), robotsTxt()],
 });
